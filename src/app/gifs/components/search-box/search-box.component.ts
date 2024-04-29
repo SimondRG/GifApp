597f0402b725080c911c127874c4f32f87934e0e
Simon Rojas <sdavid.rojas@hotmail.com>
2024-04-29 15:48:45 -0500

    Extracción del valor del input de busqueda con el decorador ViewChild y mostrarlo en consola

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef   } from '@angular/core';
 
 @Component({
     selector: 'gifs-search-box',
@@ -7,11 +7,22 @@ import { Component } from '@angular/core';
         <input type="text"
                class="form-control"
                placeholder="Buscar Gifs..."
+               (keyup.enter)="searchTag()"
+               #txtTagInput
         >
     `
 })
 
 export class SearchBoxComponent {
+
+    @ViewChild('txtTagInput')
+    public tagInput!: ElementRef<HTMLInputElement>;
+
     constructor() { }
+
+    searchTag() {
+        const newTag = this.tagInput.nativeElement.value;
+        console.log({newTag})
+    }
 
 }
