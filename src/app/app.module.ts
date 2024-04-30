478a680a4e7bec0329152717daf38d60d69aa83d
Simon Rojas <sdavid.rojas@hotmail.com>
2024-04-29 22:03:42 -0500

    Se realizó la petición https al servicio de Giphy api y se coloco el tipo de dato a la respuesta que devuelve la petición

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,7 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    HttpClientModule,
     GifsModule,
     SharedModule
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gifs, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsServices {
 
-    private _tagsHistory: string[] = [];
+    public gifList: Gifs[] = [];
 
-    constructor() { }
+    private _tagsHistory: string[] = [];
+    private apiKey      : string = 'vC0wAtAOerrLQLC4MVnOzP5NobxM2SGw';
+    private serviceUrl  : string = 'https://api.giphy.com/v1/gifs';
+
+    constructor( private http: HttpClient ) { }
 
     get tagsHistory(){
         return [...this._tagsHistory];
@@ -15,15 +21,27 @@ export class GifsServices {
         tag = tag.toLocaleLowerCase();
 
         if( this._tagsHistory.includes(tag) ){
-            this._tagsHistory = this._tagsHistory.filter( oldTag => oldTag != tag ) 
+            this._tagsHistory = this._tagsHistory.filter( oldTag => oldTag != tag );
         }
-        this._tagsHistory.unshift(tag)
-        this._tagsHistory = this._tagsHistory.slice(0,10)
+        this._tagsHistory.unshift(tag);
+        this._tagsHistory = this._tagsHistory.slice(0,10);
     }
     
     searchTag( tag:string ):void {
         if(tag.length === 0) return;
         this.organizeHistory(tag);
+
+        const params = new HttpParams()
+            .set('api_key', this.apiKey)
+            .set('limit', '10')
+            .set('q', tag);
+
+        this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params })
+            .subscribe( resp => { 
+                this.gifList = resp.data;
+                console.log( { gifs: this.gifList } )
+            });
+
 
         console.log(this._tagsHistory);
     }
